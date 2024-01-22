9210c59226003c24424c752f8315b27eaa333dbe
ygursu <yasemingrs@gmail.com>
2024-01-23 01:58:09 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,18 @@
+﻿
+static void Main(string[] args)
+{
+    int kisaKenar, uzunKenar, alan, cevre;
+    Console.Write("Kısa Kenar : ");
+    kisaKenar = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Uzun Kenar : ");
+    uzunKenar = Convert.ToInt32(Console.ReadLine());
+
+    alan = kisaKenar * uzunKenar;
+    cevre = 2 * (kisaKenar + uzunKenar);
+    Console.WriteLine("===============================");
+    Console.WriteLine("Dikdörtgenin Alanı : " + alan);
+    Console.WriteLine("Dikdörtgenin Çevresi : " + cevre);
+
+    Console.ReadLine();
+}
+
